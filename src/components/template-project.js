de649059280bd777d787608b0680e2ef7e77f465
ryoma <84463842+hryoma@users.noreply.github.com>
2022-05-27 09:56:52 -0400

    markdown styling

--- src/components/template-project.js
+++ src/components/template-project.js
@@ -1,16 +1,26 @@
-import { graphql, Link } from "gatsby"
 import React from "react"
+import styled from "styled-components"
+import { graphql, Link } from "gatsby"
+
 import PageLayout, {Container} from "./layout"
+
 import "../styles/markdown.css"
+
+
+const MarkdownWrapper = styled.div`
+  & p {
+    margin-bottom: 1.25rem;
+  }
+`
 
 export default function TemplateProject({ data }) {
   const project = data.markdownRemark
 
   return (
-    <PageLayout title={project.frontmatter.title} className="template-project project-node">
+    <PageLayout title={project.frontmatter.title}>
       <Container>
         <h1>{project.frontmatter.title}</h1>
-        <div className="markdown" dangerouslySetInnerHTML={{ __html: project.html }} />
+        <MarkdownWrapper dangerouslySetInnerHTML={{ __html: project.html }} />
         <div className="back-link" style={{ marginTop: "2rem" }}>
           <Link to="/portfolio/">Back to Portfolio</Link>
         </div>
