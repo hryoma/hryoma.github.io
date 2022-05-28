1ea1256ea4e7ca9b16cb3d5f99309ade26e49b1e
ryoma <84463842+hryoma@users.noreply.github.com>
2022-05-28 17:05:56 -0400

    Updated about section

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,6 @@ import Navbar from "../components/navbar"
 import Brand from "../components/brand";
 import SmIcons from "../components/sm-icons";
 
-import resumePdf from "../assets/Resume.pdf"
 
 
 const AboutMeWrapper = styled.div`
@@ -47,25 +46,11 @@ const AboutMe = () => {
     <AboutMeWrapper>
       <p>Hi! I'm Ryoma</p>
       <p>I study computer science at the University of Pennsylvania</p>
-      <p>Currently, I'm...</p>
-      <ul className="about-experiences">
-        <li>a web developer at{' '}
-          <ExtLink className={"text-blue"}
-                link={"https://pennclimateventures.org"}
-                title={"Penn Climate Ventures"}/>
-        </li>
-        <li>a co-founding CTO/full-stack developer at{' '}
-          <ExtLink className={"text-green"} link={"http://cynfolia.com"} title={"Cynfolia"}/>
-        </li>
-        <li>practicing cybersecurity on picoCTF</li>
-      </ul>
-      <p>This past summer, I was an IT intern at{' '}
-          <ExtLink className={"text-cyan"} link={"https://shopy2go.com"} title={"Shopy2Go"}/>
+      <p>Currently, I'm a software engineering intern at{' '}
+        <ExtLink className={"text-cyan"} link={"https://mathworks.com"} title={"MathWorks"}/>
       </p>
-      <p>You can check out my résumé{' '}
-        <ExtLink className="text-yellow" link={resumePdf} title={"here"}/>{' '}
-        and my projects{' '}
-        <IntLink className="text-blue" link={"projects"} title={"here"}/>!
+      <p>Check out my projects{' '}
+        <IntLink className="text-magenta" link={"projects"} title={"here"}/>!
       </p>
       <p>Feel free to reach out to me at: ryomah [at] seas.upenn.edu</p>
     </AboutMeWrapper>
