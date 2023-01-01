0fda82ced590d511fd12bd6b3470518328405879
hryoma <84463842+hryoma@users.noreply.github.com>
2023-01-01 10:07:48 -0500

    clean up build dir

--- build/_app/immutable/chunks/2-f644e0d0.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/projects/_layout.svelte-4d134098.js";export{t as component};
--- build/_app/immutable/chunks/3-c4b1efb9.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-51542bb8.js";export{t as component};
